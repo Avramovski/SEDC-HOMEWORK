807ee7bd51036e65910f1056435d2f3300c41d3a
Avramovski <avramovski04@gmail.com>
2023-12-06 18:32:56 +0100

    JavaScript domasno-1

---
+++ homework/javaScript-h.1/script.js
@@ -0,0 +1,18 @@
+console.log("homework 1");
+
+
+let phonePrice = 119.95;
+
+let phoneQuantity = 30;
+
+let ddv = 5;
+
+let onePhoneWithDdv = phonePrice + ((phonePrice * ddv) / 100);
+
+let allPhoneWithDdv = onePhoneWithDdv * phoneQuantity;
+
+let rezultat = allPhoneWithDdv
+
+console.log(rezultat);
+
+console.log(typeof(rezultat));
