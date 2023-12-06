aa1ef668a1990a186376617875a2bb693b8f0722
Avramovski <avramovski04@gmail.com>
2023-12-06 21:59:31 +0100

    JavaScript domasno-2

---
+++ homework/javaScript-h.2/script.js
@@ -0,0 +1,60 @@
+console.log(`homework 2`);
+
+let vnesiGodina = prompt(`Type your year`);
+
+let kalkulator = (vnesiGodina - 4) % 12;
+
+
+let broj0 = `Rat`;
+let broj1 = `Ox`;
+let broj2 = `Tiger`;
+let broj3 = `Rabbit`;
+let broj4 = `Dragon`;
+let broj5 = `Snake`;
+let broj6 = `Horse`;
+let broj7 = `Goat`;
+let broj8 = `Monkey`;
+let broj9 = `Rooster`;
+let broj10 = `Dog`;
+let broj11 = `Pig`;
+
+if (kalkulator == 0) {
+    alert(broj0);
+}
+
+else if (kalkulator == 1) {
+    alert(broj1);
+
+}
+
+else if (kalkulator == 2) {
+    alert(broj2);
+}
+else if (kalkulator == 3) {
+    alert(broj3);
+}
+else if (kalkulator == 4) {
+    alert(broj4);
+}
+else if (kalkulator == 5) {
+    alert(broj5);
+}
+else if (kalkulator == 6) {
+    alert(broj6);
+}
+else if (kalkulator == 7) {
+    alert(broj7);
+}
+else if (kalkulator == 8) {
+    alert(broj8);
+}
+else if (kalkulator == 9) {
+    alert(broj9);
+}
+else if (kalkulator == 10) {
+    alert(broj10);
+}
+else if (kalkulator == 11) {
+    alert(broj11);
+}
+
