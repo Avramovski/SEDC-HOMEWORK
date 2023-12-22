a5676b9f55e03763cd35e5609e9e19028e4ea37e
Avramovski <avramovski04@gmail.com>
2023-12-22 18:46:35 +0100

    domasno

--- homework/javaScript-h.5/task 2/script.js
+++ homework/javaScript-h.5/task 2/script.js
@@ -4,7 +4,12 @@ const ingredientsArray = [];
 
 for (let i = 0; i < numberOfIngredients; i++) {
     const ingredient = prompt('Enter your ingredient');
-    ingredientsArray.push(ingredient);
+    ingredientsArray.push
+        (`<ul>
+    <li>
+    ${ingredient}
+    </li>
+    </ul>`);
 }
 
 const div1 = document.getElementById("div1");
@@ -13,4 +18,4 @@ const div3 = document.getElementById("div3");
 
 div1.innerHTML = `<p>Name of the recipe: ${nameOfRecipe}</p>`;
 div2.innerHTML = `<p>I will need: ${numberOfIngredients} ingredient${numberOfIngredients > 1 ? 's' : ''}</p>`;
-div3.innerHTML = `<p>Ingredients: ${ingredientsArray.join(', ')}</p>`;
+div3.innerHTML = `<p>Ingredients: ${ingredientsArray.join(' ')}</p>`;
