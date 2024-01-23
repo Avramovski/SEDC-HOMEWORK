57e1beb33d5fabb8f586fc22e5a6d74027f05362
Avramovski <avramovski04@gmail.com>
2024-01-23 17:45:19 +0100

    domasna javaScript

--- homework/javaScript-h.8/script.js
+++ homework/javaScript-h.8/script.js
@@ -12,14 +12,14 @@ next.addEventListener('click', () => {
     table.innerHTML= '';
 })
 previous.addEventListener('click', () => {
-    clickCreate();
+    clickCreatePriv();
     table.innerHTML = '';
 });
 
  
 
 
-let counter = 1;
+let counter = 0;
 function clickCreate() {
     counter++;
     fetch(`https://swapi.dev/api/planets/?page=${counter}`)
@@ -52,7 +52,56 @@ function clickCreate() {
             }
             if (myJson.next === null) {
                 next.disabled = true;
+            } else {
+                next.disabled = false;
+            }
+
+            if (myJson.previous === null) {
+                previous.disabled = true;
+            } else {
+                previous.disabled = false;
+
             }
         })
 }
 
+
+function clickCreatePriv() {
+    counter--;
+    fetch(`https://swapi.dev/api/planets/?page=${counter}`)
+        .then(function (next) {
+            return next.json()
+        })
+        .then(function (myJson) {
+            table.innerHTML = '';
+            table.innerHTML += `
+        <thead>
+        <tr>
+        <th>Planet Name</th>
+        <th>Population</th>
+        <th>Climate</th>
+        <th>Gravity</th>
+        </tr>
+        </thead>
+        `
+
+            for (let planets of myJson.results) {
+                table.innerHTML +=
+                    `
+            <tr>
+            <td>${planets.name}</td>
+            <td>${planets.population}</td>
+            <td>${planets.climate}</td>
+            <td>${planets.gravity}</td>
+            </tr>
+            `;
+            }
+            if (myJson.previous === null) {
+                previous.disabled = true;
+            }
+
+            if (myJson.next !== null) {
+                next.disabled = false;
+            } 
+        })
+}
